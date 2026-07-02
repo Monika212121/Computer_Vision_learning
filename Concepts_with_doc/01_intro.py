ab08f64110e97ae8bac467de867a7230346f284f
M,onika212121 <mgadewar12@gmail.com>
2026-07-02 20:26:34 +0530

    load and save image

--- Concepts_with_doc/01_intro.py
+++ Concepts_with_doc/01_intro.py
@@ -3,7 +3,7 @@
 import cv2 as cv
 
 image = cv.imread('D:/Computer_Vision_learning/Concepts_with_doc/dataset/messi.jpg')
-cv.imshow('See this image!!!!!!!', image)
+cv.imshow('Original image', image)
 
 # cv.waitKey()                                      # param = 0 / None means the function waits indefinitely 
 
